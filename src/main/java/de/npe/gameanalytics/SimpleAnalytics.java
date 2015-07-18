896ac0fe9f302722c0952928d2b6911b2d46032c
NPException <npexception42@gmail.com>
2015-07-18 13:58:48 +0200

    add simple reference implementation

---
+++ src/main/java/de/npe/gameanalytics/SimpleAnalytics.java
@@ -0,0 +1,58 @@
+/**
+ * (C) 2015 NPException
+ */
+package de.npe.gameanalytics;
+
+import net.minecraft.client.Minecraft;
+
+/**
+ * @author NPException
+ *
+ */
+public class SimpleAnalytics extends Analytics {
+
+	private boolean isClient;
+
+	private final String gameKey;
+	private final String secretKey;
+	private final String build;
+
+	public SimpleAnalytics(String build, String gameKey, String secretKey) {
+		this.gameKey = gameKey;
+		this.secretKey = secretKey;
+		this.build = build;
+	}
+
+	public boolean isClient() {
+		return isClient;
+	}
+
+	public void markClientSide() {
+		isClient = true;
+	}
+
+	@Override
+	public boolean active() {
+		return Minecraft.getMinecraft().isSnooperEnabled();
+	}
+
+	@Override
+	public String gameKey() {
+		return gameKey;
+	}
+
+	@Override
+	public String secretKey() {
+		return secretKey;
+	}
+
+	@Override
+	public String build() {
+		return build;
+	}
+
+	@Override
+	public String userPrefix() {
+		return isClient ? "user" : "server";
+	}
+}
